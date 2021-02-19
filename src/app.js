c813cecbe267df191e45539a52db8f3f039b1858
João Vitor Gr <jooaovitorgr@gmail.com>
2021-02-19 16:21:31 -0300

    Todo App  API

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+const port = 3005
+const db = require('./infra/sqlite-db')
+const sqlite = require('sqlite3')
+
+app.use(bodyParser.json())
+app.use(cors())
+
+const controllerUsuario = require('./controllers/usuario-controller')
+controllerUsuario(app, db)
+
+const controllerTarefa = require('./controllers/tarefa-controller')
+controllerTarefa(app, db)
+
+app.listen(port, () => {
+    console.log(`Ouvindo em http://localhost:${port}`)
+})
+
